7db2a2c8a61f45871e258c640579c987b74cca36
Zijian <zijianhuang2000@gmail.com>
2024-10-12 16:53:44 +1000

    ok

--- projects/nmce-pipes/src/_pipes/date.pipe.spec.ts
+++ projects/nmce-pipes/src/_pipes/date.pipe.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-import { ShortDatePipe, Short24Pipe } from './date.pipe';
+import { ShortDatePipe, Short24Pipe, LiteralDatePipe, ShortTimePipe } from './date.pipe';
 import { Injector } from '@angular/core';
 import { DATE_PIPE_DEFAULT_OPTIONS, DatePipe } from '@angular/common';
-import { ShortTimePipe } from 'nmce-pipes';
+import moment from 'moment';
 
 describe('shortDatePipe', () => {
 	let shortDatePipe: ShortDatePipe;
@@ -24,13 +24,37 @@ describe('shortDatePipe', () => {
 	});
 
 	it('transformCrossTimezonesUs', () => {
-		const pipe = new ShortDatePipe('en-us', '+1000');
+		const pipe = new ShortDatePipe('en-us', '+1000'); //this pipe ignore locale
 		const dt = Date.parse('2023-12-28T15:33:44Z');
 		expect(pipe.transform(dt)).toEqual('29/12/2023');
 
 		const d2 = Date.parse('2023-12-28T11:33:44Z');
 		expect(pipe.transform(d2)).toEqual('28/12/2023');		
 	});
+
+	/**
+	 * https://angular.dev/api/common/DatePipe?tab=usage-notes
+	 */
+	it('transformDatePipeAu', () => {
+		const pipe = new DatePipe('en-au', '+1000');
+		const dt = Date.parse('2023-12-08T15:33:44Z');
+		expect(pipe.transform(dt)).toEqual('Dec 9, 2023');
+
+		const d2 = Date.parse('2023-12-28T11:33:44Z');
+		expect(pipe.transform(d2)).toEqual('Dec 28, 2023');		
+	});
+
+	/**
+	 * https://v17.angular.io/api/common/DatePipe, the default dateFormat is obviously mediumDate
+	 */
+	it('transformDatePipeUs', () => {
+		const pipe = new DatePipe('en-us', '+1000'); //this pipe ignore locale
+		const dt = Date.parse('2023-12-08T15:33:44Z');
+		expect(pipe.transform(dt)).toEqual('Dec 9, 2023');
+
+		const d2 = Date.parse('2023-12-28T11:33:44Z');
+		expect(pipe.transform(d2)).toEqual('Dec 28, 2023');		
+	});	
 });
 
 describe('WithTimezone10', () => {
@@ -66,11 +90,13 @@ describe('WithTimezoneMinus10', () => {
 	let shortDatePipe: ShortDatePipe;
 	let short24Pipe: Short24Pipe;
 	let shortTimePipe: ShortTimePipe;
+	let datePipe: DatePipe;
 	beforeEach(()=>{
 		TestBed.configureTestingModule({
 			providers: [ShortDatePipe,
 				Short24Pipe,
 				ShortTimePipe,
+				DatePipe,
 				{
 					provide: DATE_PIPE_DEFAULT_OPTIONS,
 					useValue: {timezone: '-1000', dateFormat: 'MM/dd/yy'} // dateFormat is ignored by ShortDatePipe
@@ -87,17 +113,78 @@ describe('WithTimezoneMinus10', () => {
 		const dt = Date.parse('2023-12-28T15:33:44Z');
 		expect(shortDatePipe.transform(dt)).toEqual('28/12/2023');
 		expect(short24Pipe.transform(dt)).toEqual('05:33');
-		//expect(shortTimePipe.transform(dt)).toEqual('05:33 am');
-		//const datePipe = new DatePipe('en-au', '-1000', {dateFormat: 'HH:mm'});
-		//expect(datePipe.transform(dt)).toEqual('23:33');
 
 		const dt2 = Date.parse('2023-12-28T09:33:44Z');
 		expect(shortDatePipe.transform(dt2)).toEqual('27/12/2023');		
 		expect(short24Pipe.transform(dt2)).toEqual('23:33');
-		//expect(shortTimePipe.transform(dt2)).toEqual('11:33 pm');
+	});	
+});
+
+describe('DatePipeMinus10', () => {
+	let datePipe: DatePipe;
+	beforeEach(()=>{
+		TestBed.configureTestingModule({
+			providers: [
+				DatePipe,
+				{
+					provide: DATE_PIPE_DEFAULT_OPTIONS,
+					useValue: {timezone: '-1000', dateFormat: 'shortTime'} // dateFormat is ignored by ShortDatePipe
+				}
+			]
+		});
+
+		datePipe= TestBed.inject(DatePipe);
+	});
+	
+	it('transformCrossTimezones', () => {
+		const dt = Date.parse('2023-12-28T15:33:44Z');
+		expect(datePipe.transform(dt)).toEqual('5:33 AM');
+
+		const dt2 = Date.parse('2023-12-28T09:33:44Z');
+		expect(datePipe.transform(dt2)).toEqual('11:33 PM');
 
 		const datePipe2 = new DatePipe('en-au', '-1000', {dateFormat: 'HH:mm'});
 		expect(datePipe2.transform(dt2)).toEqual('23:33');
+	});	
+});
+
+describe('literalDate', () => {
+	let datePipe: LiteralDatePipe;
+	beforeEach(()=>{
+		TestBed.configureTestingModule({
+			providers: [
+				LiteralDatePipe,
+				{
+					provide: DATE_PIPE_DEFAULT_OPTIONS,
+					useValue: {locale: 'en-au'} // dateFormat is ignored by ShortDatePipe
+				}
+			]
+		});
+
+		datePipe= TestBed.inject(LiteralDatePipe);
+	});
+	
+	it('transform', () => {
+		const dt = Date.parse('2023-12-28T15:33:44Z');
+		expect(datePipe.transform(dt)).toEqual('Friday, December 29th 2023');
+
+		const dt2 = Date.now();
+		expect(datePipe.transform(dt2)).toEqual('Today');
+		const dayMilliseconds = 24*3600000;
+		expect(datePipe.transform(dt2-dayMilliseconds)).toEqual('Yesterday');
+		expect(datePipe.transform(dt2+dayMilliseconds)).toEqual('Tomorrow');
+
+		expect(datePipe.transform(0)).toEqual('Thursday, January 1st 1970'); // 1970
+		expect(datePipe.transform(10)).toEqual('Thursday, January 1st 1970'); // 1970
+		expect(datePipe.transform(-100000000000)).toEqual('Tuesday, November 1st 1966'); // Before 1970
+	});
+
+	it('invalidInputs', ()=>{
+		expect(datePipe.transform(null, 'invalid')).toEqual('invalid');
+		expect(datePipe.transform(null)).toEqual('');
+		expect(datePipe.transform(undefined)).toEqual('');
+		expect(datePipe.transform('')).toEqual('');
+		expect(datePipe.transform('kkk')).toEqual('kkk');
 	});
 });
 
--- projects/nmce-pipes/src/_pipes/date.pipe.ts
+++ projects/nmce-pipes/src/_pipes/date.pipe.ts
@@ -1,13 +1,68 @@
 import { DatePipe } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 import moment from 'moment';
-import { DateFunc } from 'nmce-func';
+
+/**
+ * Date to Today, Tomorrow, Yesterday, dddd,MMMMDoYYYY
+ */
+@Pipe({ name: 'literalDate' })
+export class LiteralDatePipe implements PipeTransform {
+	/**
+	 *
+	 * @param value
+	 * @param nullText what text to shown if the date is not defined or too small.
+	 */
+	transform(value: Date | string | number | undefined | null, nullText?: string): string {
+		if (!value && value !== 0) {
+			return nullText ? nullText : '';
+		}
+
+		let date: Date;
+
+		if (typeof value === 'string') {
+			const n = Number.parseInt(value);
+			if (n < 1000) {
+				return nullText ? nullText : '';
+			}
+
+			if (Number.isNaN(n)){
+				return value;
+			}
+
+			date = new Date(n);
+		} else if (typeof value === 'number'){
+			try{
+			date=new Date(value);
+			} catch (e){
+				return value.toString();
+			}
+		} 
+		else {
+			date = value;
+		}
+
+		try {
+			return moment(date).calendar(null, {
+				sameDay: '[Today]',
+				nextDay: '[Tomorrow]',
+				nextWeek: 'dddd, MMMM Do YYYY',
+				lastDay: '[Yesterday]',
+				lastWeek: 'dddd, MMMM Do YYYY',
+				sameElse: 'dddd, MMMM Do YYYY'
+			});
+
+		} catch (e) {
+			return '';
+		}
+	}
+}
 
 //date pipes of ng2 and 4 not working in Firefox on Android, also,
 //These pipes were developed when ng date pipe was still poor. Now ng date pipe is fairly good, so these home-made pipes will be obsolete soon.
 
 /**
  * For displaying utc2local time in hh:mm a/pm format.
+ * @deprecated use shortTime of Angular DatePipe which takes dateFormat and timezone, and DATE_PIPE_DEFAULT_OPTIONS, or declare dateFormat format in pipe parameters via constant or variable
  */
 @Pipe({ name: 'shortTime' })
 export class ShortTimePipe extends DatePipe {
@@ -28,7 +83,7 @@ export class Short24Pipe extends DatePipe {
 
 /**
  * For 2-way transformation of DD/MM/YYYY
- * @deprecated use Angular DatePipe which takes dateFormat and timezone, and DATE_PIPE_DEFAULT_OPTIONS.
+ * @deprecated use shortDate of Angular DatePipe which takes dateFormat and timezone, and DATE_PIPE_DEFAULT_OPTIONS, or declare dateFormat format in pipe parameters via constant or variable
  */
 @Pipe({ name: 'shortDate' })
 export class ShortDatePipe extends DatePipe {
@@ -40,6 +95,7 @@ export class ShortDatePipe extends DatePipe {
 
 /**
  * For 2-way transformation of local DD/MM/YYYY hh:mm a
+ * @deprecated use short of Angular DatePipe which takes dateFormat and timezone, and DATE_PIPE_DEFAULT_OPTIONS, or declare dateFormat format in pipe parameters via constant or variable
  */
 @Pipe({ name: 'shortDateTime' })
 export class ShortDateTimePipe implements PipeTransform {
@@ -62,6 +118,7 @@ export class ShortDateTimePipe implements PipeTransform {
 
 /**
  * For 2-way transformation of local DD/MM/YYYY HH:mm
+ * @deprecated use Angular DatePipe which takes dateFormat and timezone, and DATE_PIPE_DEFAULT_OPTIONS, or declare dateFormat format in pipe parameters via constant or variable
  */
 @Pipe({ name: 'shortDate24' })
 export class ShortDate24Pipe implements PipeTransform {
@@ -84,6 +141,7 @@ export class ShortDate24Pipe implements PipeTransform {
 
 /**
  * Date to LLL
+ * @deprecated
  */
 @Pipe({ name: 'mediumDate' })
 export class MediumDatePipe implements PipeTransform {
@@ -98,6 +156,7 @@ export class MediumDatePipe implements PipeTransform {
 
 /**
  * Date to dddd, MMMM Do YYYY. Obsolete, to be replaced by Angular pipe date:'fullDate'
+ * @deprecated
  */
 @Pipe({ name: 'fullDate' })
 export class FullDatePipe implements PipeTransform {
@@ -109,52 +168,3 @@ export class FullDatePipe implements PipeTransform {
 		return moment(value).format('dddd, MMMM Do YYYY');
 	}
 }
-
-/**
- * Date to Today, Tomorrow, Yesterday, dddd,MMMMDoYYYY
- */
-@Pipe({ name: 'literalDate' })
-export class LiteralDatePipe implements PipeTransform {
-	/**
-	 *
-	 * @param value
-	 * @param nullText what text to shown if the date is not defined or too small.
-	 */
-	transform(value: Date | string, nullText?: string): string {
-		if (!value) {
-			return nullText ? nullText : '';
-		}
-
-		let date: Date;
-
-		if (typeof value === 'string') {
-			const n = Number.parseInt(value);
-			if (n < 1000) {
-				return nullText ? nullText : '';
-			}
-
-			date = new Date(n);
-		} else {
-			if (value.getTime() < 1000) {
-				return nullText ? nullText : '';
-			}
-
-			date = value;
-		}
-
-		try {
-			return moment(date).calendar(null, {
-				sameDay: '[Today]',
-				nextDay: '[Tomorrow]',
-				nextWeek: 'dddd, MMMM Do YYYY',
-				lastDay: '[Yesterday]',
-				lastWeek: 'dddd, MMMM Do YYYY',
-				sameElse: 'dddd, MMMM Do YYYY'
-			});
-
-		} catch (e) {
-			return '';
-		}
-	}
-}
-
