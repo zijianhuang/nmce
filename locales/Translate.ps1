83d8b9751c56d9a3677d8d3edc43cecce091ead3
Z <zijianhuang2000@gmail.com>
2025-06-12 08:24:23 +1000

    ok

--- locales/Translate.ps1
+++ locales/Translate.ps1
@@ -1,8 +1,8 @@
 Set-Location $PSScriptRoot
 $commandPath = 'C:/Green/GoogleTranslateXliff/GoogleTranslateXliff.exe'
-$apiKey = 'AIzaSyAmb9tAxFgxuex4WcnHQvbRpwHpE-GoYws'
+$apiKeyFile = 'C:/VsProjects/OpenSource/Secrets/GoogleTranslate'
 
 foreach ($lang in 'es', 'ja', 'zh-hans', 'zh-hant') {
-    $cmd = '$commandPath /AK=$apiKey /B /F=messages.$lang.xlf'
+    $cmd = '$commandPath /AKF=$apiKeyFile /B /F=messages.$lang.xlf'
     Invoke-Expression $ExecutionContext.InvokeCommand.ExpandString($cmd)
 }
