de404e9226ce6081a5745000521e9ad035259aa2
Z <zijianhuang2000@gmail.com>
2025-11-08 05:41:37 +1000

    ok

--- projects/nmce-func/src/_func/uuidFunc.ts
+++ projects/nmce-func/src/_func/uuidFunc.ts
@@ -1,4 +1,4 @@
-import { v4 as uuid } from 'uuid'; //todo: somehow  the system could not recognize v5.
+import { v4 as uuidv4 } from 'uuid'; //todo: somehow  the system could not recognize v5.
 // https://github.com/DefinitelyTyped/DefinitelyTyped/pull/28439
 
 export class UuidFunc {
@@ -6,11 +6,11 @@ export class UuidFunc {
 	 * 36 UUID string including 4 hyphens. MySql stores GUID as 36 bytes anyway rather than 16bytes.
 	 */
 	static newUUID(): string {
-		return uuid();
+		return uuidv4();
 	}
 
 	static newUUIDStartWith0() {
-		const s = uuid();
+		const s = uuidv4();
 		return '0000' + s.slice(4);
 	}
 }
