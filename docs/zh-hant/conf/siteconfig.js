752e457fd7d3515b851f571e18389f2effb5db3e
Z <zijianhuang2000@gmail.com>
2026-02-14 17:32:32 +1000

    themes

---
+++ docs/zh-hant/conf/siteconfig.js
@@ -0,0 +1,8 @@
+const SITE_CONFIG = {
+	themesDic: {
+		"assets/themes/rose-red.css":{display: "Roes & Red", dark:false},
+		"assets/themes/azure-blue.css":{display: "Azure & Blue", dark:false},
+		"assets/themes/magenta-violet.css":{display: "Magenta & Violet", dark:true},
+		"assets/themes/cyan-orange.css":{display: "Cyan & Orange", dark:true}
+	}
+}
